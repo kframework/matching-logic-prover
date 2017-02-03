fd591d64d743f8ecd9a5d2d878a70bf22f564156
Xiaohong <xiaohong993@foxmail.com>
2017-02-03 11:06:00 -0600

    update ml2pl

--- ml2pl/src/Pattern.java
+++ ml2pl/src/Pattern.java
@@ -1,20 +1,20 @@
-import javax.swing.tree.*;
-
 /**
- * Created by xiaohong on 2/2/17.
+ * Created by Xiaohong on 2/3/17.
  */
-public class Pattern{
-    private DefaultTreeModel pattern;
+public abstract class Pattern {
+    private Sort sort;
 
     /**
-     * Creates a pattern which is a variable named @varName in sort @s.
-     * @param varName
-     * @param s
+     * Resolve sorting information for pattern and its subpatterns.
      */
-    public Pattern(String varName, Sort s){
-        DefaultMutableTreeNode root = new DefaultMutableTreeNode(varName, false);
-        this.pattern = new DefaultTreeModel(root, true);
+    protected abstract void resolveSorting();
+
+    /**
+     * Get the sort of a pattern.
+     * @return Sort of the pattern.
+     */
+    public Sort getSort() {
+        resolveSorting();
+        return sort;
     }
-
-
 }
---
+++ ml2pl/src/TopSort.java
@@ -0,0 +1,5 @@
+/**
+ * Created by xiaohong on 2/3/17.
+ */
+public class TopSort {
+}
