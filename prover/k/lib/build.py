3f245dad685e0c4e7665a5a4acb10a73a53798f6
Nishant Rodrigues <nishantjr@gmail.com>
2019-01-25 03:00:35 +0530

    t/lsegleft-implies-list: Mark passing

--- prover/k/lib/build.py
+++ prover/k/lib/build.py
@@ -55,11 +55,11 @@ mlprover = proj.source('matching-logic-prover.md') \
 do_prove('unit-tests', mlprover, 'UNIT-TESTS-SPEC', 'unit-tests.md').default()
 do_test(mlprover, 't/emptyset-implies-isempty.prover').default()
 do_test(mlprover, 't/lsegleft-implies-lsegright.prover').default()
+do_test(mlprover, 't/lsegleft-implies-list.prover').default()
 
 # Failing tests:
 # These tests aren't passing yet. To mark as passing append `.default()`
 # and move to above section.
-do_test(mlprover, 't/lsegleft-implies-list.prover')
 do_test(mlprover, 't/bst-implies-bt.prover')
 do_test(mlprover, 't/LTL-Ind.prover') # can be fixed
 
